587b3435936a24494399939d154a41f9bda35f10
Miguel Angel Cruz Gangoitia <miguelangelcruz@MacBook-Pro-de-Miguel.local>
2023-12-28 12:03:14 +0100

    update dist folder

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,13 @@
+//import axios from "axios"
 
 const URL = window.location.hostname === `localhost`
   ? `http://localhost:3040`
-  : `https://172.16.5.4:5173`
+  : `http://144.126.206.220`
 
+// const customInstance = axios.create({
+//   baseURL: URL,
+//   headers: {'Accept': 'aplication/json'}
+// })
+
+//export default customInstance
 export { URL }
